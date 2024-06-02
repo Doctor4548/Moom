368f58d476e9a8ed86a17bb54496a1889aaaa272
Doctor4548 <tianxiang.yang99@qmail.cuny.edu>
2024-06-02 11:05:13 -0400

    uppercase

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -31,7 +31,7 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
         <Image src={img} alt="meeting" width={27} height={27} />
       </div>
-      
+
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-bold">{title}</h1>
         <p className="text-lg font-normal">{description}</p>
@@ -40,7 +40,7 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
   );
 };
 
-const page = () => {
+const RootPage = () => {
   const now = new Date();
   const router = useRouter();
   const { toast } = useToast()
@@ -229,4 +229,4 @@ const page = () => {
   )
 }
 
-export default page
+export default RootPage
